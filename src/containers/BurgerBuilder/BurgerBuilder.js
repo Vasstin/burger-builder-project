dcc0d6d2b9d2c0da172c0ab78c06e43809814b51
Vasstin <vastin115408@gmail.com>
2021-01-12 15:19:34 +0200

    added orders, improved chackout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,33 +85,12 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // this.setState({loading: true})
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Max Schwarc',
-    //     address: {
-    //       street: "Teststreet 1",
-    //       zipCode: '41354',
-    //       country: 'Gremany'
-    //     },
-    //     email: 'test@gmail.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // }
-    // axios.post('/orders.json', order)
-    //   .then(response => {
-    //     this.setState({loading:false, purchasing: false})
-    //   })
-    //   .catch(error => {
-    //     this.setState({loading:false, purchasing: false})
-    //   })
     const querryParams = [];
     for(let i in this.state.ingredients) {
       querryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
     }
 
+    querryParams.push('price=' + this.state.totalPrice)
     const querryString = querryParams.join('&')
 
     this.props.history.push({
