1ddb1b57fd3a86d82769cc1c0b16ce63d7df3fd7
Vasstin <vastin115408@gmail.com>
2021-01-15 15:21:22 +0200

    added validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,7 +16,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       street: {
         elementType: 'input',
@@ -24,7 +28,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Street'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       zipCode: {
         elementType: 'input',
@@ -32,7 +40,13 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your ZIP Code'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false
       },
       country: {
         elementType: 'input',
@@ -40,7 +54,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       email: {
         elementType: 'input',
@@ -48,7 +66,11 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'Your E-Mail'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -58,7 +80,7 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'Cheapest'}
         ]
         },
-        value: ''
+        value: '' 
       }
     },
     loading: false
@@ -67,9 +89,15 @@ class ContactData extends Component {
   orderHandler = (event) => {
     event.preventDefault();
     this.setState({loading: true})
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+    }
+
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
+      orderData: formData
     }
     axios.post('/orders.json', order)
       .then(response => {
@@ -80,26 +108,57 @@ class ContactData extends Component {
         this.setState({loading:false})
       })
   }
-  
+
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid 
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+    }
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm})
+  }  
+
   render() {
-    const fromElementsArray = [];
+    const formElementsArray = [];
     for (let key in this.state.orderForm) {
-      fromElementsArray.push({
+      formElementsArray.push({
         id: key,
         config: this.state.orderForm[key]
       })
     }
-    console.log(fromElementsArray)
     let form = (
-      <form>
-        {fromElementsArray.map(fromElement => (
+      <form onSubmit = {this.orderHandler}>
+        {formElementsArray.map(formElement => (
           <Input 
-            key = {fromElement.id}
-            elementType = {fromElement.config.elementType}
-            elementConfig = {fromElement.config.elementConfig}
-            value = {fromElement.config.value}/>
+            key = {formElement.id}
+            elementType = {formElement.config.elementType}
+            elementConfig = {formElement.config.elementConfig}
+            value = {formElement.config.value}
+            changed = {(event) => this.inputChangedHandler(event, formElement.id)}/>
         ))}
-        <Button btnType = "Success" clicked = {this.orderHandler}>ORDER</Button>
+        <Button btnType = "Success">ORDER</Button>
       </form>);
     if (this.state.loading) {
       form = <Spinner/>
